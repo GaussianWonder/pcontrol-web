f161e014960d94d540bd4fbabe56e86fdc6403b8
Virghileanu Teodor <teo.virghi@yahoo.ro>
2021-03-26 01:23:52 +0200

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:vue/strongly-recommended',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'vue',
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2,
+    ],
+    'quotes': [
+      'error',
+      'single',
+    ],
+    'semi': [
+      'error',
+      'always',
+    ],
+    'comma-dangle': [
+      'error',
+      'always-multiline',
+    ],
+    'no-trailing-spaces': [
+      'error',
+      {
+        'ignoreComments': true,
+      },
+    ],
+    'no-empty': 0,
+    'no-unused-vars': 0,
+    'no-undef': 0,
+    'no-mixed-spaces-and-tabs': 0,
+    'vue/no-v-for-template-key': [
+      'ignore',
+    ],
+    'vue/no-v-model-argument': [
+      'ignore',
+    ],
+  },
+};
